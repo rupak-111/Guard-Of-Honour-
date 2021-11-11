3181ebcf9062c60a9cb4da60042f4185a3dc35d6
kenny-nishi <87802856+kenny-nishi@users.noreply.github.com>
2021-11-11 15:00:15 +0800

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#inlcude "head.h"
 using namespace std;
 
+
+struct output Q4(struct input)
+{
+  int x,y;
+  struct output out;
+  //10 *10 range of coordinates
+  x=rand()%10+1;
+  y=rand()%10+1;
+  cout<<x<<y;
+  return out;
+}
