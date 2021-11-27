ce850b4967aac07bc8fe8882ce9e1bf6a33006a2
kenny-nishi <87802856+kenny-nishi@users.noreply.github.com>
2021-11-27 14:14:20 +0800

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -7,11 +7,28 @@ using namespace std;
 
 struct output Q4(struct input)
 {
-  int x,y;
-  struct output out;
-  //10 *10 range of coordinates
-  x=rand()%10+1;
-  y=rand()%10+1;
-  cout<<x<<y;
-  return out;
+    int x, y;
+    output out = {0};
+    srand(time(0));
+   int  LENGTH = rand() % 50 + 1;
+    x = rand() % LENGTH + 1;
+    y = rand() % LENGTH + 1;
+    cout << "   ";
+    for (int i = 1; i <= LENGTH; i++)
+        cout <<setw(3) << i ;
+    cout << endl;
+    for (int i = 1; i <= LENGTH; i++)
+        for (int j = 1; j <= LENGTH; j++)
+        {
+            if (j == 1)
+                cout << setw(3) << i;
+            cout << setw(3) << ((i == x && j == y) ? 'B' : 'b') << (j == LENGTH ? '\n' : '\0');
+        }
+    int xans, yans;
+    do {
+        cout << "What is the coordinates of 'B' ?  type in x_coordinate and y_coordinate: ";
+        cin >>xans >> yans;
+    } while (xans != x || yans != y);
+    cout << "You answer correctly, the answer of coordinates are " << x <<" and " << y;
+    return out;
 }
