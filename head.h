53db7c4a2599c47bcab7990329d89bdbf5c0417c
kenny-nishi <87802856+kenny-nishi@users.noreply.github.com>
2021-12-03 00:38:57 +0800

    Update head.h

--- head.h
+++ head.h
@@ -4,33 +4,38 @@
 #include<iomanip>
 #include<string>
 
-static int bonus;
-static int live;
+static int Bonus;
+static int live=3;
 static bool bonus_double = false;
-struct input {
-    int health;
+   
+int Q2_ans=100;
+string Q3_ans="DCAB"
 
-};
+struct Q4{int x, int y};//have return value
 
-struct output {
-    char Char;
-    string String;
-    double Double;
-    int Int;
-    int bonus_point;
+int Q5_int=9;
+char Q5_char='m';
+string Q5_string="BETA";
+    
+int q6;//have return value
 
-};
-output proceed_game_stage(int gamenumber);
-output q1(void);
-output q2(void);
-output q3(void);
-output q4(void);
-output q5(void);
-output q6(void);
-output q7(void);
-output q8(void);
-output q9(void);
-output q10(void);
+int q7;//have return value
+
+
+
+
+
+void proceed_game_stage(int gamenumber);
+void q1(void);
+int q2(void);
+string q3(void);
+Q4 q4(void);
+void q5(void);
+int q6(void);
+int q7(void);
+void q8(void);
+void q9(void);
+void q10(void);
 
 void bonus_system(void);
 
@@ -56,4 +61,22 @@ static int Checkinteger_positive(void)
             std::cin.ignore(80, '\n');
         }
     }
+}
+
+int check_integer(int ans)
+{
+    cout << "Please enter an integer. \n";
+    int answer;
+    while (true)
+        {
+            if (cin >> answer)
+            {
+                if(answer==ans)
+                    break;
+            cout << "Incorrect. Try again.\n";                 
+            }
+            cout << "Please enter an integer. \n";
+            std::cin.clear();
+            std::cin.ignore(80, '\n');
+        }
 }
