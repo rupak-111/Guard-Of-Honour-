94fca1b73bb46c92cfd01d0f490927bcb51d3715
rupak-111 <93245638+rupak-111@users.noreply.github.com>
2021-12-02 13:36:34 +0800

    Update and rename Numbershape.cpp to q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -21,17 +21,28 @@ int main()
         cout<< "                  3 2 1\n";
         cout<< "                  3 2\n";
         cout<< "                  3 2\n";
-        cout<< "                   4\n";
+        cout<< "                   3\n";
         
     
     cout<< "\n\n\nEnter the key of the door :->  ";
-    cin>> answer;
-    for (int i = 0; answer!= 38  || answer < 0; i++)
+    while(true)
     {
-        cout<< "Please enter positive integer";
-        cin>> answer;
+        if(cin>> answer)
+        {
+            if(answer==100)
+                break;
+            else if(answer>0)
+                cout<<"Sorry, you get wrong, try again\n";
+            else 
+                cout<<"Please input a positive integer:\n";
+        }
+        else
+        {
+            std::cin.clear();
+            std::cin.ignore(80, '\n');
+            cout<<"Please input a positive integer:\n";
+        }
     }
-    if ( answer== 100 )
         cout<<"\n\n Correct!\nRemember the passcode as you might need it later!!"<<endl;
     return 0;
 }
