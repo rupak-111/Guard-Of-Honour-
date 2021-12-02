46d842dd0fa6339bfa0efeef3032eb07a523474b
rupak-111 <93245638+rupak-111@users.noreply.github.com>
2021-12-02 13:33:08 +0800

    Update Numbershape.cpp

--- Numbershape.cpp
+++ Numbershape.cpp
@@ -1,27 +1,37 @@
 #include<iostream>
-#include<unistd.h>
-#include<ctime>
+
 using namespace std;
 
 int main()
 {
+    cout<< "In this level you have to find the key of the door by adding all the numbers present in the figure:\n\n";
     int answer;
-    cout<< "";
-    for (int i=0; i<5; i++)
-    {
        
-        cout << "\n\n\n                  3  4\n                2      5  ";
+        cout << "\n                  3  4\n                2      5  ";
         cout<< "\n              1          1 \n            2             3";
         cout<< "\n           2 1 2 3 2 1 3 2 1 \n";
-        system ("clear");
-    }
-    cout<< "Enter the key of the door :->  ";
+        cout<< "                  3 2\n";
+        cout<< "                  3 2\n";
+        cout<< "                  3 2\n";
+        cout<< "                  3 2\n";
+        cout<< "                  3 2 1 1 1\n";
+        cout<< "                  3   1 1\n";
+        cout<< "                  3 2 1\n";
+        cout<< "                  3   1 1\n";
+        cout<< "                  3 2 1\n";
+        cout<< "                  3 2\n";
+        cout<< "                  3 2\n";
+        cout<< "                   4\n";
+        
+    
+    cout<< "\n\n\nEnter the key of the door :->  ";
     cin>> answer;
-    if (answer != (int) answer || answer < 0)
+    for (int i = 0; answer!= 38  || answer < 0; i++)
     {
         cout<< "Please enter positive integer";
+        cin>> answer;
     }
-    if ( answer== 38 )
+    if ( answer== 100 )
         cout<<"\n\n Correct!\nRemember the passcode as you might need it later!!"<<endl;
     return 0;
 }
