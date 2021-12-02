99b33291c9e29b158bbd695002609204d7652c2f
kenny-nishi <87802856+kenny-nishi@users.noreply.github.com>
2021-12-02 11:52:56 +0800

    Update head.h

--- head.h
+++ head.h
@@ -1,11 +1,57 @@
-struct input {
-  double Time;
-  int health;
-};
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include<iomanip>
+#include<string>
 
-struct output{
-  union answer {string, int, double};
-  double Time;
-  int bonus point;
-  int health;
+static int bonus;
+static int live;
+static bool bonus_double = false;
+struct input {
+    int health;
+
 };
+int maze(void);
+struct output {
+    //union answer {string, int, double};
+    double Time;
+    int bonus_point;
+    int health;
+};
+output proceed_game_stage(int gamenumber);
+output q1(void);
+output q2(void);
+output q3(void);
+output q4(void);
+output q5(void);
+output q6(void);
+output q7(void);
+output q8(void);
+output q9(void);
+output q10(void);
+
+void bonus_system(void);
+
+static int Checkinteger_positive(void)
+{
+    int numbers;
+
+    while (true)
+    {
+        if (std::cin >> numbers)
+        {
+            if(numbers>0)
+                return numbers;
+            else
+            {
+                std::cout << "Type positive integers number please ";
+            }
+        }
+        else if(std::cin.fail())
+        {
+            std::cout << "Please enter a positive integer: ";
+            std::cin.clear();
+            std::cin.ignore(80, '\n');
+        }
+    }
+}
