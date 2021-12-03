cd978816e6a56fd9c01f1f633fa063a889e4be55
kenny-nishi <87802856+kenny-nishi@users.noreply.github.com>
2021-12-03 13:42:36 +0800

    Update BOSS.cpp

--- BOSS.cpp
+++ BOSS.cpp
@@ -1,13 +1,14 @@
 #include"head.h"
 using namespace std;
-int BOSS(void)
+void BOSS(void)
 {
-	cout << "Arthur has successfully overcome all the obstacles to get to the monster. Now only thing that is left is to kill the monster and get back the sword.\n";
-	cout << "Will he able to do it? Help Arthur on this final quest:\n\n";
+	//stage1
 	//introduction
 	
 	//code of stage1
 	check_integer(100);//to check whether the answer input is 100 or not 
+	cout << "You success open the door";//try to change it to more smooth
+
 	//stage2
 	// introduction
 	
@@ -20,7 +21,7 @@ int BOSS(void)
 		for (int i = 0; i < ans.length(); i++)
 		{
 			if (ans[i] <= 'z' || ans[i] >= 'a')
-				ans[i] = toupper(ans[i);
+				ans[i] = toupper(ans[i]);
 		}
 		//if equal, then go out of loop 
 		if (ans == Q5_string)
@@ -30,6 +31,7 @@ int BOSS(void)
 	cout<<"The boss name is \"BETA\""<<endl;//may be you can change this if you liked
 	//stage3
 	// introduction
+	
 	// code
 	cout << "input the x-coordinates :";
 	int x = Checkinteger_positive();
@@ -42,6 +44,7 @@ int BOSS(void)
 		else
 			cout << "It was wrong, do you remember where is the 'B' coordinates?\n";
 	}
-	//stage4
-	return 0;
+	//ending
+	//you kill the boss, and finished the game
+
 }
