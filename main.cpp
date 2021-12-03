9afe08a5f9ccc1313765fb8733692a71b83b5d30
rupak-111 <93245638+rupak-111@users.noreply.github.com>
2021-12-03 14:26:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-	cout << "Welcome to this game";
+	cout << "Welcome to this game. It is an adventurous game. The story plot- the king's valuable as well as the magical sword is stolen by a demon, and nobody is successful to get back the sword to the king\n.";
+	cout << "Now king trusts Arthur to get it back. Arthur is getting prepared to set out on the journey to get back the king's sword, but he does not have any idea, or what he will encounter about the journey because those who tried, nobody came back alive.\n";
+	cout <<  "Arthur is alone, and starts this perilious adventure at midnight.\n";
+	cout << "After crossing a snowy desert, he gets to see the Demon's island.  Arthur is about to encounter ten different obstacles and the sooner he is scuccessful overcoming them, the more bonus points he gets.\n";
+	cout << "After overcoming all the obstacles, he gets to meet the monstor who stole the king's sword. Arthur has to fight intensely to kill the monster.\n\n\n";
+	cout << "Lets start the adventure: \n" <<endl;
+	
 	//to let user to load or new game
 	cout << "\nPress key 1 to have a new game or press 2 to load in a file to start from the previous stage\n";
 	int choose;
