3ee5e6e276fe5f116faeb4d27bfd758a2a29520e
rupak-111 <93245638+rupak-111@users.noreply.github.com>
2021-12-03 14:16:30 +0800

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int q2(void)
 {
+    // Guidelines for user
     cout << "Congratulation on completing first level. Arthur, with additional health bonus on his backpack, sets out on the path that leads him into the dense jungle. " << endl;
     cout << "The jungle is so thick that not even the light passes through it. After walking continuously for eight hours, he finally gets to see the rising sun.\n";
     cout << "But suddenly his leg is trapped by a monstrous vein and he is swung upside down 60 meters above the ground." << endl;
     cout << "The only way to get Arthur safe on the ground is to answer the question written on the stem of the vein which is also supposed to be the passcode of the monste'rs den." << endl;
     cout << "Help Arthur to unlock himself from the vein: \n\n";
+    
+    //code
     int answer;
-
+    
     cout << "\n                  3  4\n                2      5  ";
     cout << "\n              1          1 \n            2             3";
     cout << "\n           2 1 2 3 2 1 3 2 1 \n";
@@ -25,10 +28,11 @@ int q2(void)
     cout << "                  3 2 1\n";
     cout << "                  3 2\n";
     cout << "                  3 2\n";
-    cout << "                   3\n";
+    cout << "                   2\n";
 
 
     cout << "\n\n\nEnter the key of the door :->  ";
+    //if user inputs the correct answer, s/he proceeds to next level
     while (true)
     {
         if (cin >> answer)
@@ -40,6 +44,7 @@ int q2(void)
             else
                 cout << "Please input a positive integer:\n";
         }
+        //clearing the cache memory
         else
         {
             std::cin.clear();
