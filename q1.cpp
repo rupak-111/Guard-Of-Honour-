57f517e5c9950fe944d5ab0e2d61cf87fbf26ba2
rupak-111 <93245638+rupak-111@users.noreply.github.com>
2021-12-03 14:10:17 +0800

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -7,10 +7,12 @@ void q1(void)
     cout << "\nArthur can only kill the cockroach by beating it on RockScissorsPaper Game.";
     cout << "\nHelp Arthur to win this level. Remember, the faster you beat the cockroach, the more health bonus Arthur gets:";
 
-
+    
     int trial = 3;
     int bonus = 0;
     cout << "You can get 10 bonus if win, 5 bounu point if lose and 0 point for lose";
+    
+    //code
     while (trial--)
     {
         srand(time(NULL));
@@ -22,7 +24,7 @@ void q1(void)
 
         int choiceOfComputer = (rand() % 3) + 1;
 
-
+        //using loop for conditions
         while (true)
         {
             if (cin >> choiceOfYou && choiceOfYou < 4 && choiceOfYou>0)
@@ -47,7 +49,7 @@ void q1(void)
             cout << "Computer chose Scissors." << endl;
         cout << endl;
 
-
+        //For bonus point
         if (choiceOfYou == choiceOfComputer)
         {
             cout << "StaleMate!\n\n";
@@ -55,8 +57,12 @@ void q1(void)
         }
         if (choiceOfYou == 1)
         {
+            
+            //when user chooses rock and computer chooses paper
             if (choiceOfComputer == 2)
                 cout << "You Lost!\n" << endl;
+            
+            //when user chooses rock and computer chooses scissors
             else if (choiceOfComputer == 3)
             {
                 cout << "You Won!\n" << endl;
@@ -71,13 +77,17 @@ void q1(void)
                 cout << "You Won!\n" << endl;
                 bonus += 10;
             }
+            //when user chooses paper and computer chooses rock
             else if (choiceOfComputer == 3)
                 cout << "You Lost!\n" << endl;
         }
+        //when user chooses scissors and computer chooses rock
         else if (choiceOfYou == 3)
         {
             if (choiceOfComputer == 1)
                 cout << "You Lost!\n" << endl;
+            
+            //when user chooses scissors and computer chooses paper
             else if (choiceOfComputer == 2)
             {
                 cout << "You Won!\n" << endl;
